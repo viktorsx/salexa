d3280457df14a2bbd74885ca304fdf26efb6db68
Viktor <viktors@vikto.rs>
2024-07-01 14:59:24 +0200

    Test

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 
           <PatientForm />
           <div className="text-14-regular mt-20 flex justify-between">
-          <p className="justify-items-end text-dark-600 xl:text-left">© 2024 Carepulse</p>
+          <p className="justify-items-end text-dark-600 xl:text-left">© 2024 Carepulse+</p>
           <Link href="/?admin=true" className="text-green-500">
           Admin
           </Link>
