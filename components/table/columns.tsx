1d25d5cb1250cc98b5b3fb45defb79150cb1d32a
Viktor <45247501+viktorsx@users.noreply.github.com>
2025-01-28 04:57:03 +0100

    test2

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -54,7 +54,7 @@ export const columns: ColumnDef<Appointment>[] = [
         return (
           <div className="flex items-center gap-3">
             <Image
-              src={doctor?.image}
+              src={doctor?.image!}
               alt="doctor"
               width={100}
               height={100}
