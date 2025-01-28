775d6ea82f565c76c349d4394e1543fc1f38d38c
Viktor <45247501+viktorsx@users.noreply.github.com>
2025-01-28 04:29:31 +0100

    tes

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -7,9 +7,6 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://71fec8d416a23237ec35da979a815d26@o4508718890352640.ingest.de.sentry.io/4508718893957200",
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 });
