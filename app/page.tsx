339cc675e814c21639a1ecd91d6dd398cda782ed
Viktor <45247501+viktorsx@users.noreply.github.com>
2025-01-28 21:16:55 +0100

    finally okay

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
+
 import PatientForm from "@/components/forms/PatientForm";
 import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
 
-export default function Home() {
-  const searchParams = useSearchParams();
-  const isAdmin = searchParams.get("admin") === "true";
+export default function Home({ searchParams}: SearchParamProps) {
+  const isAdmin = searchParams.admin === 'true';
 
   return (
     <div className="flex h-screen max-h-screen">
@@ -15,19 +14,19 @@ export default function Home() {
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
-            src="/assets/icons/logo-full.svg"
-            height={1200}
-            width={1200}
-            alt="patient"
-            className="mb-12 h-12 w-fit"
+          src="/assets/icons/logo-full.svg"
+          height={1200}
+          width={1200}
+          alt="patient"
+          className="mb-12 h-12 w-fit"
           />
 
           <PatientForm />
           <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">© 2024 Carepulse+</p>
-            <Link href="/?admin=true" className="text-green-500">
-              Admin
-            </Link>
+          <p className="justify-items-end text-dark-600 xl:text-left">© 2024 Carepulse+</p>
+          <Link href="/?admin=true" className="text-green-500">
+          Admin
+          </Link>
           </div>
         </div>
       </section>
@@ -38,7 +37,8 @@ export default function Home() {
         width={1000}
         alt="patient"
         className="side-img max-w-[50%]"
+
       />
     </div>
-  );
+  )
 }
