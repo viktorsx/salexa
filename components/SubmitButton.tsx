68622ae5242c1a36fc118727ac9b322d791bfd35
Viktor <45247501+viktorsx@users.noreply.github.com>
2025-01-28 04:16:57 +0100

    Sentry implemented

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -12,11 +12,7 @@ interface ButtonProps {
 
 const SubmitButton = ({ isLoading, className, children}: ButtonProps) => {
     return (
-        <Button
-            type="submit"
-            disabled={isLoading}
-            className={`${className ?? 'shad-primary-btn w-full'} ${isLoading ? 'disabled-btn' : ''}`}
-        >
+        <Button type="submit" disabled={isLoading} className={className ?? 'shad-primary-btn w-full'}>
             {isLoading ? (
                 <div className="flex items-center gap-4">
                     <Image
